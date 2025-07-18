d10a771ee5ebf88ae7206668781436db9530bb11
bilawal-dev <bk4449719@gmail.com>
2025-07-19 02:43:14 +0500

    Ignore EsLint Issues During Build

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ['upload.wikimedia.org'],
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
