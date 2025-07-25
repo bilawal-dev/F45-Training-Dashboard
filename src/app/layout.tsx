4070234872ac4e0b12299f09518a953608816a14
bilawal-dev <bk4449719@gmail.com>
2025-07-26 04:06:30 +0500

    useSearchParams Import Issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import ChatwootLoader from "./ChatWootLoader";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "F45 Dashboard - Project Management System",
@@ -12,7 +13,9 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
     <html suppressHydrationWarning={true} lang="en">
       <body suppressHydrationWarning={true} className="antialiased">
 
-        <ChatwootLoader />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ChatwootLoader />
+        </Suspense>
 
         {children}
       </body>
